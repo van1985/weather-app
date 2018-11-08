768603c4f3fb18c37617e6a24d4fada764a9dac6
leandro.garcia <leandro.garcia@globant.com>
2018-11-08 17:13:13 -0300

    Fix inline styles

--- src/components/searchInput/searchInput.js
+++ src/components/searchInput/searchInput.js
@@ -16,8 +16,7 @@ class SearchInput extends Component {
          placeholder="Enter a city..."
          ref={input => this.search = input}
        />
-       <div onClick={this.handleInputChange} 
-       style={{display:'inline-block',backgroundColor:'#282c34',width:'130px',height:'35px',color:'white',border:'1px solid #6494c4',cursor:'pointer'}} >Search</div>
+       <div onClick={this.handleInputChange} className="searchInput-btn">Search</div>
      </form>
    )
  }
